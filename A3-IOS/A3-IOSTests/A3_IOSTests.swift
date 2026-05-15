263b90038ed6696dbbfa5ad42cb6e95e4b85567a
mhtruong-00 <mhtruong@utas.edu.au>
2026-05-15 11:50:07 +1000

    update

---
+++ A3-IOS/A3-IOSTests/A3_IOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  A3_IOSTests.swift
+//  A3-IOSTests
+//
+//  Created by mobiledev on 15/5/2026.
+//
+
+import Testing
+@testable import A3_IOS
+
+struct A3_IOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ A3-IOS/A3-IOSUITests/A3_IOSUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  A3_IOSUITestsLaunchTests.swift
+//  A3-IOSUITests
+//
+//  Created by mobiledev on 15/5/2026.
+//
+
+import XCTest
+
+final class A3_IOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
